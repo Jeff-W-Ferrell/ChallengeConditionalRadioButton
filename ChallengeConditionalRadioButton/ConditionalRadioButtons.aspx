7f8c23d1cbe3071984e0bb1e8d019257666586c4
Jeff Ferrell <jwferrell78@gmail.com>
2017-10-20 14:35:57 -0700

    Latest Version with a note
    Conditional radio buttons with images.

---
+++ ChallengeConditionalRadioButton/ConditionalRadioButtons.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ConditionalRadioButtons.aspx.cs" Inherits="ChallengeConditionalRadioButton.ConditionalRadioButtons" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Your Note Taking Preferences<strong><br />
+        </strong>
+        <br />
+        <asp:RadioButton ID="pencilRadioButton" runat="server" GroupName="notationImplements" Text="Pencil" />
+        <br />
+        <asp:RadioButton ID="penRadioButton" runat="server" GroupName="notationImplements" Text="Pen" />
+        <br />
+        <asp:RadioButton ID="phoneRadioButton" runat="server" GroupName="notationImplements" Text="Phone" />
+        <br />
+        <asp:RadioButton ID="tabletRadioButton" runat="server" GroupName="notationImplements" Text="Tablet" />
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Image ID="resultImage" runat="server" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeConditionalRadioButton/ConditionalRadioButtons.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton
+{
+    public partial class ConditionalRadioButtons : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            /* resultLabel.Text = (pencilRadioButton.Checked) 
+                ? "You selected Pencil"
+                : "Please select your preffered notaton implement */
+            if (pencilRadioButton.Checked)
+            {
+                resultLabel.Text = "You selected Pencil.";
+                resultImage.ImageUrl = "pencil.png";
+            }
+            else if (penRadioButton.Checked)
+            {
+                resultLabel.Text = "You selected Pen.";
+                resultImage.ImageUrl = "pen.png";
+            }
+            else if (phoneRadioButton.Checked)
+            {
+                resultLabel.Text = "You selected Phone.";
+                resultImage.ImageUrl = "phone.png";
+            }
+            else if (tabletRadioButton.Checked)
+            {
+                resultLabel.Text = "You selected Tablet.";
+                resultImage.ImageUrl = "tablet.png";
+            }
+            else
+            {
+                resultLabel.Text = "Please select your preferred notation implement.";
+            }
+            /*The tutorial actually had a mistake where the unselected resultlabel.Text
+              displayed with the conditional image selections*/
+        }
+    }
+}
